4931307b50605e21e3f4ec2015782e42335765f3
Kai Blumberg <kblumberg@email.arizona.edu>
2020-10-28 12:14:19 +0100

    Add script to standardize basepair values to be in G (basepairs)

---
+++ job_list/clean_sample_metadata/parse_base_pairs/base_pairs.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+Author : kai
+Date   : 2020-10-28
+Purpose: Rock the Casbah
+
+run: ./base_pairs.py -i bases_in.csv -o bases_out.csv
+"""
+
+import argparse
+import sys
+import csv
+import re
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Argparse Python script',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # parser.add_argument(
+    #     'positional', metavar='str', help='A positional argument')
+
+
+    parser.add_argument(
+        '-i',
+        '--input',
+        help='input csv',
+        metavar='str',
+        type=str,
+        default='')
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        help='output csv',
+        metavar='str',
+        type=str,
+        default='')
+
+    parser.add_argument(
+        '-f', '--flag', help='A boolean flag', action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    in_arg = args.input
+    out_arg = args.output
+
+    input_list = []
+
+    # open input.csv
+    with open(in_arg, mode ='r', encoding='utf-8-sig') as input:
+        csv_reader = csv.reader(input, delimiter=',')
+        for row in csv_reader:
+            input_list.append(row[0])
+
+
+    regex_basepairs = r"(\d*[.]?\d+)([a-z|A-Z])"
+    div_basepairs = '1000'
+
+    f = open(out_arg, 'a')
+
+    for x in input_list:
+        if re.search(regex_basepairs, str(x)):
+            match = re.search(regex_basepairs, str(x))
+            #print(match.group(1), match.group(2))
+            number = match.group(1)
+            letter = match.group(2)
+
+            if letter == 'G':
+                print(number,file=f)
+            if letter == 'M':
+                print(round(float(number)/float(div_basepairs),4),file=f)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
